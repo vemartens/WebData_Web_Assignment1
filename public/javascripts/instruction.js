62f0e5a8f1ff9601ef0ec3745c730d90e6946b28
Floors <F.F.straver@student.tudelft.nl>
2018-12-09 11:36:56 +0100

    instructions

--- public/javascripts/instruction.js
+++ public/javascripts/instruction.js
@@ -1,7 +1,7 @@
 
 function instructionButton() {
     
-    var textString = "";
+    var textString = "The Codemaker sets a secret code, then the Codebreaker tries to match the code using logic, deduction, and maybe even a little bit of luck. After each move, the Codemaker must give clues that help the Codebreaker. Can you crack the code in 10 moves or less.. and can you come up with a code that will last for 10 moves? A white pin by checking means that one is correct, but not in the right place and a red pin means that one is correct and in the right place. ";
     
     alert(textString);
 }
